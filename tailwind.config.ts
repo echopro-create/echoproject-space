3d41961072edf0ba97e1137ad1515fbb75ee9ec8
Ilya <your@email>
2025-09-23 00:18:54 +0200

    chore: config cleanup (use *.cjs), ensure scripts

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss";
-export default {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx,jsx}", "./lib/**/*.{ts,tsx}"],
-  theme: { extend: { borderRadius: { '2xl': '1rem' } } },
-  darkMode: "media",
-  plugins: [],
-} satisfies Config;
