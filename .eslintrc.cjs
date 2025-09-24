851648a9bae4ea2adb1ffa44dd3929a57b4926c7
Ilya <your@email>
2025-09-24 18:39:24 +0200

    chore(eslint): минимальный конфиг без mojibake

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
-﻿// .eslintrc.cjs
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
-  rules: {
-    // апрещаем кракозябры
-    'no-mojibake': 'error',
-  },
-  plugins: ['mojibake'],
 };
