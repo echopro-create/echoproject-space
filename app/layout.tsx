da12aa06e9e52110fe8d2fbda4b0de9c975a3961
Ilya <your@email>
2025-09-26 21:23:21 +0200

    fix(css): правильный импорт глобальных стилей из ../styles/styles.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './styles.css';
+﻿import "../styles/styles.css";
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'ECHO',
-  description: 'Послания в будущее',
+  description: 'РџРѕСЃР»Р°РЅРёСЏ РІ Р±СѓРґСѓС‰РµРµ',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
